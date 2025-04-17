8760be2ee4d62b76073f9660c2c5e16033fb11d0
Rohith krishnan <rohithkrishnan997@gmail.com>
2025-04-17 17:16:06 +0530

    check

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 const PORT =3000
 
 app.use(express.static(path.join(__dirname,'views')))
-
+//1
 
 app.listen(PORT,()=>{
     console.log(`server running at http://localhost:${PORT}`)
